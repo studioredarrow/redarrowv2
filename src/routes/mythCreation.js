a6e6163329c3c11939bf1f190721244fd542e457
Developmentexpert121 <developmentexpert121@gmail.com>
2025-12-31 10:30:06 +0530

    changes in the mythcreation file

--- src/routes/mythCreation.js
+++ src/routes/mythCreation.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("mythCreation", {
+  res.render("pages/mythCreation", {
     title: "Myth Creation"
   });
 });
