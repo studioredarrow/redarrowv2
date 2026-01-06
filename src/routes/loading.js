f3c7116b948fca65dbf2c6a48546c900c7b65a09
Developmentexpert121 <developmentexpert121@gmail.com>
2026-01-06 13:01:45 +0530

    added loading and menu page

--- src/routes/loading.js
+++ src/routes/loading.js
@@ -1,26 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { client } = require("../prismic");
 
-router.get("/loading", async (req, res) => {
-  try {
-    const doc = await client.getSingle("loading_page");
+router.get("/loading", (req, res) => {
+  const { to = "/", label = "" } = req.query;
 
-    const loading = {
-      title: doc.data.title,
-      description: doc.data.description,
-    };
-
-    res.render("pages/loading", {
-      title: "Loading",
-      loading,
-      isHome: false,
-      showFooter: false
-    });
-  } catch (err) {
-    console.error("Prismic loading page error:", err);
-    res.status(500).send("CMS Error");
-  }
+  res.render("pages/loading", {
+    title: "Loading",
+    targetUrl: to,
+    label,
+    isHome: false,
+    showFooter: false
+  });
 });
 
 module.exports = router;
